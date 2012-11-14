d9549fa0849a224ca247e0bfbc3a36be35c30ce1
Stephen Roller <roller@cs.utexas.edu>
2012-11-14 14:41:30 +0100

    Turns out I wasn't filtering everything I need to filter.

--- python/comp/whole_analysis.py
+++ python/comp/whole_analysis.py
@@ -2,13 +2,40 @@
 
 import sys
 import pandas as pd
+from collections import Counter
+
+fake_words = set("""
+    Analigzerbruch Armmoder Brattlider Bulkerzagen Engschogen Fennhoder Harmweg
+    Luderschwege Malligwohmer Pillinrugen Quetpfluge Tropebuhle Wierzverkuhr
+    Zogschucht
+""".split())
+
+def remove_fakes(data):
+    fake_judgements = data[data.compound.map(fake_words.__contains__)]
+    failed_workers = set(data[data.judgement != 8].workerId)
+
+    without_fakes = data[data.workerId.map(failed_workers.__contains__)]
+    sys.stderr.write("Before removing fakes: %d\tafter: %d\n" % (len(data), len(without_fakes)))
+    return without_fakes
+
 
 def parse_whole_judgements(file):
-    data = pd.read_csv(file, names=('workerId', 'stuff', 'stuff2', 'compound', 'judgement'))
-    data = data.fillna(8)
-    # don't need these columns
-    del data['stuff']
-    del data['stuff2']
+    data = pd.read_csv(file, names=('workerId', 'worktime', 'approval', 'compound', 'judgement'))
+    # remove everyone who failed our fake word tests
+    data = remove_fakes(data)
+    # only keep people with an approval of at least 90.
+    N = len(data)
+    data.approval = data.approval.map(lambda x: int(x.split()[0]))
+    data = data[data.approval >= 90]
+    sys.stderr.write("Before approval rating: %d\tafter: %d\n" % (N, len(data)))
+    # Now we only want to keep people who made at least 10 judgements
+    user_counts = Counter(data.workerId)
+    good_users = set(k for k, v in user_counts.iteritems() if v >= 10)
+    N = len(data)
+    data = data[data.workerId.map(good_users.__contains__)]
+    sys.stderr.write("Before min count: %d\tafter: %d\n" % (N, len(data)))
+
+
     # filter out nonanswers
     data = data[data.judgement != 8]
     # group into users
