9b175f3a931ac10219ce567ad79202e183617ee8
Stephen Roller <roller@cs.utexas.edu>
2012-11-19 19:14:40 +0100

    Remove Tau measure. Delete dead code. Factor out the loading of data.

--- python/comp/eval_cleanups.py
+++ python/comp/eval_cleanups.py
@@ -128,6 +128,7 @@ class FillCleaner(Cleaner):
     def __str__(self):
         return "Fill w %d" % self.fill_value
 
+
 def decrange(start, stop, inc):
     out = []
     x = start
@@ -151,65 +152,42 @@ def combine_measures(agg_heads_and_mods, method='prod'):
     together['compound'] = together.index
     return together
 
-# def rho_with_wholes(indiv, whole):
-#     assert (indiv['compound'] == whole['compound']).all()
-# 
-#     rho, p = spearmanr(indiv['mean'], whole['mean'])
-#     # rho, p = kendalltau(indiv['mean'], whole['mean'])
-#     # hardcode statistical significance
-#     if not p < .05:
-#         #import pdb
-#         #pdb.set_trace()
-#         sys.stderr.write("Uh oh, got a p < .05 (%f)\n" % p)
-#     return rho, p
-# 
-# def rho_with_assoc(indiv, assoc, measure):
-#     assert (indiv['compound'] == assoc['compound']).all()
-#     assert (indiv['const'] == assoc['const']).all()
-#     assert measure in ('jaccard', 'cosine')
-#     # return kendalltau(indiv['mean'], assoc[measure])
-#     return spearmanr(indiv['mean'], assoc[measure])
-# 
-# 
-# 
+def load_data():
+    heads = pd.read_csv(HEAD_FILE)
+    mods = pd.read_csv(MOD_FILE)
+    whole = pd.read_csv(WHOLE_FILE).sort('compound')
+    assoc = pd.read_csv(ASSOC_FILE)
 
+    # make sure we drop the association measures between a compound
+    # and itself. they're always 1.
+    assoc = assoc[assoc.compound != assoc.const]
+    assoc = assoc.sort(['compound', 'const'])
 
-heads = pd.read_csv(HEAD_FILE)
-mods = pd.read_csv(MOD_FILE)
-whole = pd.read_csv(WHOLE_FILE)
-assoc = pd.read_csv(ASSOC_FILE)
+    # we can only work with the intersection of all 3 files in terms of
+    # judgements
+    good_compounds = reduce(
+        set.intersection,
+        [set(x.compound) for x in [heads, mods, whole, assoc]]
+    )
+    heads, mods, whole, assoc = [
+        d[d.compound.map(good_compounds.__contains__)]
+        for d in [heads, mods, whole, assoc]
+    ]
 
-# make sure we drop the association measures between a compound
-# and itself. they're always 1.
-assoc = assoc[assoc.compound != assoc.const]
-
-# we can only work with the intersection of all 3 files in terms of
-# judgements
-good_compounds = reduce(
-    set.intersection,
-    [set(x.compound) for x in [heads, mods, whole, assoc]]
-)
-heads, mods, whole, assoc = [
-    d[d.compound.map(good_compounds.__contains__)]
-    for d in [heads, mods, whole, assoc]
-]
+    return heads, mods, whole, assoc
 
 
 # go ahead and sort the whole judgements and assoc measures
-whole_orig = whole.sort('compound')
+heads, mods, whole_orig, assoc = load_data()
 whole = aggregate_ratings(whole_orig)
-assoc = assoc.sort(['compound', 'const'])
 concatted = pd.concat([heads, mods], ignore_index=True)
-
 
 setups = []
 setups += [BaselineCleaner()]
-setups += [RemoveDeviantSubjectCleaner(r) for r in decrange(0.20, 0.6, 0.05)]
-setups += [RemoveDeviantRatings(z) for z in decrange(1.0, 4.0, 0.5)]
-#setups += [EloRatings(k, 1500, 400) for k in [.001, .05, .1, 1, 5, 10, 30]]
 setups += [RemoveDeviantSubjectCleaner(r) for r in decrange(0.10, 0.6, 0.05)]
+#setups += [RemoveDeviantRatings(z) for z in decrange(1.0, 4.0, 0.5)]
 setups += [RebinCleaner(b) for b in ["1144477","1444447","1114777","1122233","1222223","1112333"]]
-setups += [SvdCleaner(k) for k in range(1, 31)]
+setups += [SvdCleaner(k) for k in range(1, 11)]
 setups += [FillCleaner(0), FillCleaner(1), FillCleaner(7)]
 
 results = []
@@ -227,31 +205,23 @@ for cleaner in setups:
     sys.stderr.write("Finished cleaning head/const ratings. (%s)\n" % cleaner)
     whole_clean = aggregate_ratings(cleaner.scores(whole_orig)).sort('compound')
     sys.stderr.write("Finished cleaning whole ratings. (%s)\n" % cleaner)
-    row_rho = cleaner.parameters()
-    row_rho['metric'] = 'rho'
-    row_tau = cleaner.parameters()
-    row_tau['metric'] = 'tau'
-    parameters.update(row_rho.keys())
+    row = cleaner.parameters()
+    parameters.update(row.keys())
 
     together = combine_measures(agg, 'prod').sort('compound')
     rho, p1 = spearmanr(together['mean'], whole['mean'])
     tau, p2 = kendalltau(together['mean'], whole['mean'])
-    row_rho['whole'] = rho
-    row_tau['whole'] = tau
+    row['whole'] = rho
 
     # and now when we clean up whole measures
     rho, p1 = spearmanr(together['mean'], whole_clean['mean'])
     tau, p2 = kendalltau(together['mean'], whole_clean['mean'])
-    row_rho['whole cleaned'] = rho
-    row_tau['whole cleaned'] = tau
+    row['whole cleaned'] = rho
 
     rho, p1 = spearmanr(agg['mean'], assoc['jaccard'])
-    tau, p2 = kendalltau(agg['mean'], assoc['jaccard'])
-    row_rho['association sim'] = rho
-    row_tau['association sim'] = tau
+    row['association sim'] = rho
 
-    results.append(row_rho)
-    results.append(row_tau)
+    results.append(row)
 
 results = pd.DataFrame(results)
 output = melt(results, id_vars=parameters)
@@ -260,7 +230,6 @@ output.to_csv(sys.stdout, index=False)
 # produce plots
 from rplots import line_plot
 import operator
-parameters.remove('metric')
 for p in parameters:
     other_params = parameters - set([p])
     if other_params:
@@ -270,7 +239,7 @@ for p in parameters:
         experiment = output
     line_plot("graphs/" + p + ".pdf", experiment, p, 'value', 'variable',
             ylab='Resulting Correlation',
-            linetype='metric', linename="Metric", colorname="Eval Method")
+            colorname="Eval Method")
 
 
 
