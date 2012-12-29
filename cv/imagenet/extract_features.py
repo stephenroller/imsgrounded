de604df4de68eb63b8d25e7bb22e1bcbb60a665e
Stephen Roller <roller@cs.utexas.edu>
2012-12-30 00:37:59 +0100

    Sample features first time through.

--- cv/imagenet/extract_features.py
+++ cv/imagenet/extract_features.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env python
 
 import sys
 import bz2file
@@ -8,6 +8,7 @@ import PIL
 import argparse
 import os.path
 from lxml import etree
+from random import random
 
 #from progress import ProgressBar
 
@@ -116,6 +117,8 @@ def main():
             if whitelist and wnid not in whitelist:
                 continue
             for feature in extractor(cimg):
+                if random() > .01:
+                    continue
                 fstr = " ".join(map(repr, feature))
                 ofile.write("%s\t%s\t%s\n" % (wnid, cropid, fstr))
         #ofile.flush()
