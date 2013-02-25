ae3a7187628b1a6367d9813c518b355e20a539f7
Stephen Roller <roller@cs.utexas.edu>
2013-02-25 12:19:51 +0100

    Deal with big vectors much better.

--- cv/in2bywords.py
+++ cv/in2bywords.py
@@ -4,12 +4,20 @@ import sys
 import pandas as pd
 import numpy as np
 import os.path
+import bz2
 from math import sqrt
 from scipy.stats import spearmanr
 
 storedir = "/Users/stephen/Working/imsgrounded/data"
 
-vectors = pd.read_table(sys.argv[1], header=False, names=("Synset", "Vector"))
+#vectors = pd.read_table(sys.argv[1], header=False, names=("Synset", "Vector"))
+if sys.argv[1].endswith(".bz2"):
+    f = bz2.BZ2File(sys.argv[1])
+else:
+    f = open(sys.argv[1])
+
+lines = [line.split("\t") for line in f.read().split("\n") if line.strip()]
+vectors = pd.DataFrame([{'Synset': a, 'Vector': b} for a, b in lines])
 
 
 mappings_raw = pd.read_table(os.path.join(storedir, "images/imagenet_mappings.tsv"))
@@ -26,7 +34,11 @@ def norm1(v):
     return v
     return v / float(sum(v))
 
-parsed = vectors.Vector.map(lambda x: norm1(np.array(map(float, x.split(" ")))))
+try:
+    parsed = vectors.Vector.map(lambda x: norm1(np.array(map(int, x.split(" ")))))
+except:
+    parsed = vectors.Vector.map(lambda x: norm1(np.array(map(float, x.split(" ")))))
+
 del vectors["Vector"]
 vectors["Vector"] = parsed
 
