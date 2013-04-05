2ff864ec4055414c9ae207146d27d01b11a35137
Stephen Roller <roller@cs.utexas.edu>
2013-04-05 06:12:51 -0500

    Stuff for graphing.

--- aesir/nicemodel.py
+++ aesir/nicemodel.py
@@ -15,12 +15,19 @@ def load_labels(filename):
     if not filename:
         return {}
     else:
-        with open(filename) as f:
+        with codecs.getreader('utf-8')(open(filename)) as f:
             lines = (l.strip().split() for l in f.readlines() if l.strip())
             return {int(i):w for i,w in lines}
 
 def ranked_list(probabilities, n):
     return sorted(enumerate(probabilities), key=lambda x: x[1], reverse=True)[:n]
+
+def pad_same(column, extra=2):
+    width = max(len(l) for l in column) + extra
+    return [l + (width - len(l)) * " " for l in column]
+
+def column_join(list_of_columns, connector=' | '):
+    return '\n'.join(connector.join(line) for line in zip(*list_of_columns))
 
 def main():
     parser = argparse.ArgumentParser(description='Outputs a human readable model.')
@@ -34,8 +41,12 @@ def main():
                         help='The vocab labels.')
     parser.add_argument('--features', '-f', metavar='FILE',
                         help='The feature labels.')
-    parser.add_argument('--docs', '-d', action='store_true',
-                        help='Output the document distributions.')
+    parser.add_argument('--docs', '-D', metavar='FILE',
+                        help='Output the document distributions for these documents.')
+    parser.add_argument('--docids', '-d', metavar='FILE',
+                        help='The document labels.')
+    parser.add_argument('--detailedtopics', '-p', action='store_true',
+                        help='Output nice, human readable information about documents.')
     args = parser.parse_args()
 
     model = np.load(args.model)
@@ -48,34 +59,85 @@ def main():
     # phi is vocab
     # psi is features
     # pi is documents
-    if args.topics:
+
+    topic_strings = {}
+
+    if args.topics or args.detailedtopics:
         for k in xrange(model['k']):
             bestphi = ranked_list(model['phi'][k], TOPIC_WORDS_SHOW)
             bestpsi = ranked_list(model['psi'][k], TOPIC_FEATS_SHOW)
 
-            print "Topic %d:" % k
-            print "  Phi (vocab):"
+            topic_str = []
+            topic_str.append("Topic %d:" % k)
+            topic_str.append("  Phi (vocab):")
             for i, p in bestphi:
-                print "    %.5f  %s" % (p, label_vocab.get(i, "word_%d" % i))
-            print "  Psi (features):"
+                topic_str.append("    %.5f  %s" % (p, label_vocab.get(i, "word_%d" % i)))
+            topic_str.append("  Psi (features):")
             for i, p in bestpsi:
-                print "    %.5f  %s" % (p, label_features.get(i, "feat_%d" % i))
+                topic_str.append("    %.5f  %s" % (p, label_features.get(i, "feat_%d" % i)))
 
-            print
+            if args.topics:
+                print '\n'.join(topic_str)
+            if args.detailedtopics:
+                topic_strings[k] = pad_same(topic_str)
+
+
+
+    if args.docs:
+        docids = codecs.getreader('utf-8')(open(args.docids)).readlines()
+        docids = (d[:d.rindex('/')] for d in docids)
+        docids = {dname: dnum for dnum, dname in enumerate(docids)}
+        whitedocs = list(codecs.getreader('utf-8')(open(args.docs)).read().split())
+        for docname in whitedocs:
+            try:
+                docid = docids[docname]
+            except KeyError:
+                pass
+            docdist = model['pi'][docid]
+            if not args.detailedtopics:
+                docdist_s = ' '.join(map(repr, docdist))
+                #print "%s\t%s" % (docname, docdist_s)
+                for i, p in enumerate(docdist):
+                    print "%s,%d,%f" % (docname, i, p)
+            else:
+                # output nice stuff.
+                sorted_dist = sorted(list(enumerate(docdist)), key=lambda x: x[1], reverse=True)
+                sorted_dist = [(i, p) for i, p in sorted_dist if p > 1e-6]
+                print "Document: %s" % docname
+                for i, p in sorted_dist:
+                    print "  Topic %d: %f" % (i, p)
+                print column_join([topic_strings[k] for k, p in sorted_dist])
+                print
+
+
 
     if args.words:
-        whitewords = set(open(args.words).read().split())
-        mappings = []
+        whitewords = codecs.getreader('utf-8')(open(args.words)).read().split()
+        mappings = {}
         for k,v in label_vocab.iteritems():
-            if v[:v.rindex("/")] in whitewords and '/NN' in v:
-                mappings.append(k)
+            nicev = v[:v.rindex("/")]
+            if nicev in whitewords and '/NN' in v:
+                mappings[nicev] = k
 
-        for m in mappings:
-            word = unicode(label_vocab[m], 'utf-8')
+        for ww in whitewords:
+            if ww not in mappings:
+                continue
+            m = mappings[ww]
+            word = label_vocab[m]
             probs = model['phi'][:,m]
             probs = probs / np.sum(probs)
-            print word[:word.rindex("/")] + "\t" + " ".join(str(p) for p in probs)
-
+            niceword = word[:word.rindex("/")]
+            if args.detailedtopics:
+                niceprobs = [(i, p) for i, p in enumerate(probs) if p > 1e-4]
+                print "Word: %s" % niceword
+                for i, p in niceprobs:
+                    print "  Topic %d: %f" % (i, p)
+                print column_join([topic_strings[k] for k, p in niceprobs])
+                print
+            else:
+                #print word[:word.rindex("/")] + "\t" + " ".join(str(p) for p in probs)
+                for i, p in enumerate(probs):
+                    print "%s,%d,%f" % (niceword, i, p)
 
 
 if __name__ == '__main__':
