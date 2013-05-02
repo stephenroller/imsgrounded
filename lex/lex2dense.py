d1b5d0b80ea3c88c9e1b25906751eed78de50146
Stephen Roller <roller@cs.utexas.edu>
2013-05-02 16:07:18 -0500

    Support removing columns without adequate counts.

--- lex/lex2dense.py
+++ lex/lex2dense.py
@@ -3,26 +3,30 @@ import pandas as pd
 import numpy as np
 import sys
 from util import *
+from collections import Counter
+
+MIN_COUNT = 0
 
 f = openfile(sys.argv[1])
 f.next()
 data = {}
 
-cols = set()
+cols = Counter()
 tars = set()
 for line in f:
     t, c, v = line.strip().split("\t")
     t, pos = extract_word_pos(t)
     if pos != 'NN':
-        continue
+        #continue
+        pass
     if t not in data:
         data[t] = {}
     assert c not in data[t], "already found context %s for target %s" % (c, t)
     data[t][c] = v
-    cols.add(c)
+    cols.update([c])
     tars.add(t)
 
-cols = sorted(cols)
+cols = sorted(w for w, v in cols.iteritems() if v >= MIN_COUNT)
 tars = sorted(tars)
 
 for t in tars:
