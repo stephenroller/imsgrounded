4eef049bbcfa61cf7a105ff56fff72f58b328825
Stephen Roller <roller@cs.utexas.edu>
2013-05-12 14:44:46 -0500

    Don't die on words that appear with multiple POS tags.

--- lex/lex2dense.py
+++ lex/lex2dense.py
@@ -6,6 +6,8 @@ from util import *
 from collections import Counter
 
 MIN_COUNT = 0
+ADD_MULTIPLE = True
+IGNORE_NONNN = True
 
 f = openfile(sys.argv[1])
 f.next()
@@ -14,15 +16,23 @@ data = {}
 cols = Counter()
 tars = set()
 for line in f:
+    if IGNORE_NONNN and '/NN' not in line:
+        continue
     t, c, v = line.strip().split("\t")
+    v = float(v)
     t, pos = extract_word_pos(t)
     if pos != 'NN':
         #continue
         pass
     if t not in data:
         data[t] = {}
-    assert c not in data[t], "already found context %s for target %s" % (c, t)
-    data[t][c] = v
+    if c in data[t]:
+        if ADD_MULTIPLE:
+            data[t][c] += v
+        else:
+            raise ValueError, 'Already found context "%s" for target "%s".' % (c, t)
+    else:
+        data[t][c] = v
     cols.update([c])
     tars.add(t)
 
@@ -30,7 +40,7 @@ cols = sorted(w for w, v in cols.iteritems() if v >= MIN_COUNT)
 tars = sorted(tars)
 
 for t in tars:
-    as_str = " ".join(data[t].get(c, "0") for c in cols)
+    as_str = " ".join(repr(data[t].get(c, 0)) for c in cols)
     print "%s\t%s" % (t, as_str)
 
 
