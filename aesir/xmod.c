8fa7c4a5352cfe4db3925b0846645793d75adcfe
Stephen Roller <roller@cs.utexas.edu>
2013-05-17 02:38:51 -0500

    Online stochastic variational inference LDA.

--- aesir/xmod.c
+++ aesir/xmod.c
@@ -43,6 +43,43 @@ typedef struct {
 int NUM_CORES;
 int NUM_TOPICS;
 
+static PyObject* digamma(PyObject *self, PyObject *args) {
+  float x;
+  if (!PyArg_ParseTuple(args, "f", &x)) {
+    return NULL;
+  }
+
+  float p;
+  if (x == 0.0)
+    p = MAGIC_GAMMA_CONSTANT;
+  else
+    p = fastdigamma(x);
+
+
+  return PyFloat_FromDouble(p);
+}
+
+static void v_digamma(char **args, npy_intp *dimensions, npy_intp *steps, void *data) {
+  npy_intp i;
+  npy_intp n = dimensions[0];
+  char *in = args[0], *out = args[1];
+  npy_intp in_step = steps[0], out_step = steps[1];
+
+  double tmp;
+
+  for (i = 0; i < n; i++) {
+    // BEGIN main ufunc computation
+    tmp = *(double*)in;
+    if (tmp < GAMMA_THRESH)
+      *((double *)out) = MAGIC_GAMMA_CONSTANT;
+    else
+      *((double *)out) = fastdigamma(tmp);
+    // END main ufunc computation
+    in += in_step;
+    out += out_step;
+  }
+}
+
 static inline float fasttrigamma(float x) {
   float p;
   x=x+6;
@@ -371,9 +408,13 @@ static PyMethodDef xmod_methods[] = {
   {"xfactorialposterior", xfactorialposterior, METH_VARARGS},
   {"initialize", initialize, METH_VARARGS},
   {"finalize", finalize, METH_NOARGS},
+  {"digamma", digamma, METH_VARARGS},
   {NULL, NULL} // required ending of the method table
 };
 
+PyUFuncGenericFunction digamma_funcs[1] = {&v_digamma};
+static char types[2] = {NPY_DOUBLE, NPY_DOUBLE};
+static void *data[1] = {NULL};
 
 PyMODINIT_FUNC initxmod() {
   PyObject *mod = Py_InitModule("xmod", xmod_methods);
@@ -381,6 +422,13 @@ PyMODINIT_FUNC initxmod() {
 
   // required NumPy initialization */
   import_array();
+  // okay, let's get our ufunc's up and running.
+  import_umath();
+
+  PyObject *d = PyModule_GetDict(mod);
+  PyObject *v_digamma = PyUFunc_FromFuncAndData(digamma_funcs, data, types, 1, 1, 1, PyUFunc_None, "vdigamma", "vectorized digamma", 0);
+  PyDict_SetItemString(d, "vdigamma", v_digamma);
+  Py_DECREF(v_digamma);
 }
 
 
