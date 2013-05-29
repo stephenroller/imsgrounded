649209dbdf37a74de15874403bcbf992f963e8ff
Stephen Roller <roller@cs.utexas.edu>
2013-05-29 17:37:56 -0500

    Half blown evaluation, but merges some things.

--- aesir/compclass.py
+++ aesir/compclass.py
@@ -4,9 +4,11 @@ import sys
 import argparse
 import numpy as np
 import pandas as pd
+import scipy.stats
+import logging
 from itertools import combinations
 from aesir import row_norm
-from assoctest import jsdiv
+from assoctest import jsdiv, kldiv
 from nicemodel import load_labels
 
 def col_norm(a):
@@ -17,61 +19,105 @@ comp_file = '/home/01813/roller/tmp/imsgrounded/data/comp/comp-values_all_sorted
 
 def main():
     parser = argparse.ArgumentParser(description='Outputs a human readable model.')
-    parser.add_argument('--model', '-m', metavar='FILE',
-                        help='The saved model.')
+    parser.add_argument('models', metavar='FILE', help='The saved models.', nargs='+')
     parser.add_argument('--vocab', '-v', metavar='FILE',
                         help='The vocab labels.')
+    parser.add_argument('--acc-thresh', type=float, default=0,
+                        help="Don't include pairwise comparisons whose judgements are closer than this threshold.")
     args = parser.parse_args()
-
-    phi = np.ascontiguousarray(np.load(args.model)['phi'])
-    topic_normed = col_norm(phi)
-    word_normed = row_norm(phi)
-
-    vocab_labels = load_labels(args.vocab)
-    vocab_labels = {w : i for i, w in vocab_labels.iteritems()}
 
     comp_tab = pd.read_table(comp_file, encoding='utf-8')
     comp_tab = comp_tab[comp_tab['const'] != comp_tab['compound']]
 
-    baseline_correct = 0
-    jsdiv_correct = 0
-    w2w1_correct = 0
-    w1w2_correct = 0
-    pairs_compared = 0
-    for (i, pair1), (j, pair2) in combinations(comp_tab.iterrows(), 2):
-        try:
-            cnst1_id = vocab_labels[pair1['const'] + '/NN']
-            cnst2_id = vocab_labels[pair2['const'] + '/NN']
-            cmpd1_id = vocab_labels[pair1['compound'] + '/NN']
-            cmpd2_id = vocab_labels[pair2['compound'] + '/NN']
-        except KeyError:
-            continue
+    vocab_labels = load_labels(args.vocab)
+    vocab_labels = {w : i for i, w in vocab_labels.iteritems()}
 
-        if pair1['mean'] == pair2['mean']:
-            continue
+    model_evaluations = []
+    for model in args.models:
+        logging.info("Processing model '%s'..." % model)
+        m = np.load(model)
+        k = m['k']
+        ll = np.mean('loglikelihoods' in m and m['loglikelihoods'][-5:] or m['perwordbounds'][-5:])
+        iter = m['max_iteration']
+        time = np.sum(m['timediffs'])
+        phi = np.ascontiguousarray(m['phi'])
+        topic_normed = col_norm(phi)
+        word_normed = row_norm(phi)
 
-        gold = pair1['mean'] < pair2['mean']
+        model_eval = {'k': k, 'll': ll, 'iter': iter, 'time': time}
 
-        cmpd1_given_cnst1 = np.dot(topic_normed[:,cmpd1_id], word_normed[:,cnst1_id])
-        cmpd2_given_cnst2 = np.dot(topic_normed[:,cmpd2_id], word_normed[:,cnst2_id])
-        cnst1_given_cmpd1 = np.dot(topic_normed[:,cnst1_id], word_normed[:,cmpd1_id])
-        cnst2_given_cmpd2 = np.dot(topic_normed[:,cnst2_id], word_normed[:,cmpd2_id])
+        similarities = {}
+        for i, pair in comp_tab.iterrows():
+            try:
+                cnst_id = vocab_labels[pair['const'] + '/NN']
+                cmpd_id = vocab_labels[pair['compound'] + '/NN']
+            except KeyError:
+                continue
 
-        pairs_compared += 1
-        baseline_correct += (gold == 1)
+            pair_k = (pair['const'], pair['compound'])
+            cmpd_given_cnst = np.dot(topic_normed[:,cmpd_id], word_normed[:,cnst_id])
+            cnst_given_cmpd = np.dot(topic_normed[:,cnst_id], word_normed[:,cmpd_id])
+            jsdiv_sim = jsdiv(word_normed[:,cmpd_id], word_normed[:,cnst_id])
+            kldiv1 = kldiv(word_normed[:,cmpd_id], word_normed[:,cnst_id])
+            kldiv2 = kldiv(word_normed[:,cnst_id], word_normed[:,cmpd_id])
 
-        cnst1 = phi[:,cnst1_id]
-        cnst2 = phi[:,cnst2_id]
-        cmpd1 = phi[:,cmpd1_id]
-        cmpd2 = phi[:,cmpd2_id]
-        jsdiv_correct += (gold == (jsdiv(cnst1, cmpd1) > jsdiv(cnst2, cmpd2)))
-        w2w1_correct += (gold == (cnst1_given_cmpd1 < cnst2_given_cmpd2))
-        w1w2_correct += (gold == (cmpd1_given_cnst1 < cmpd2_given_cnst2))
+            similarities[pair_k] = {'compound':  pair['compound'],
+                                    'const':     pair['const'],
+                                    'cmpd|cnst': cmpd_given_cnst,
+                                    'cnst|cmpd': cnst_given_cmpd,
+                                    'jsdiv':     jsdiv_sim,
+                                    'kldiv1':    kldiv1,
+                                    'kldiv2':    kldiv2,
+                                    'human':     pair['mean'],
+                                    }
 
-    print "Acc basel: %5.2f%%" % (100.0 * float(baseline_correct) / pairs_compared)
-    print "Acc jsdiv: %5.2f%%" % (100.0 * float(jsdiv_correct) / pairs_compared)
-    print "Acc w2|w1: %5.2f%%" % (100.0 * float(w2w1_correct) / pairs_compared)
-    print "Acc w1|w2: %5.2f%%" % (100.0 * float(w1w2_correct) / pairs_compared)
+        # let's compute spearman's rho for each of the measures:
+        tmp = pd.DataFrame(similarities.values())
+        for m in ['cmpd|cnst', 'cnst|cmpd', 'jsdiv', 'kldiv1', 'kldiv2']:
+            rho, p = scipy.stats.spearmanr(tmp[m], tmp['human'])
+            model_eval['rho_' + m] = rho
+            model_eval['p_' + m] = p
+
+
+
+        # okay now let's do accuracy style measures
+        baseline_correct = 0
+        jsdiv_correct = 0
+        kldiv1_correct = 0
+        kldiv2_correct = 0
+        cmpdcnst_correct = 0
+        cnstcmpd_correct = 0
+        pairs_compared = 0.0
+        for (i, pair1), (j, pair2) in combinations(comp_tab.iterrows(), 2):
+            if pair1['mean'] == pair2['mean'] or abs(pair1['mean'] - pair2['mean']) < 1.0:
+                continue
+
+            try:
+                pair1_k = (pair1['const'], pair1['compound'])
+                similarities1 = similarities[pair1_k]
+                pair2_k = (pair2['const'], pair2['compound'])
+                similarities2 = similarities[pair2_k]
+            except KeyError:
+                continue
+
+            gold = pair1['mean'] < pair2['mean']
+
+            pairs_compared += 1
+            baseline_correct += (gold == 1)
+
+            jsdiv_correct += (gold == (similarities1['jsdiv'] > similarities2['jsdiv']))
+            cmpdcnst_correct += (gold == (similarities1['cmpd|cnst'] < similarities2['cmpd|cnst']))
+            cnstcmpd_correct += (gold == (similarities1['cnst|cmpd'] < similarities2['cnst|cmpd']))
+
+        prod = 100.0 / pairs_compared
+        model_eval['acc_baseline'] = baseline_correct / pairs_compared
+        model_eval['acc_jsdiv'] = jsdiv_correct / pairs_compared
+        model_eval['acc_cmpd|cnst'] = cmpdcnst_correct / pairs_compared
+        model_eval['acc_cnst|cmpd'] = cnstcmpd_correct / pairs_compared
+
+        model_evaluations.append(model_eval)
+
+    pd.DataFrame(model_evaluations).to_csv(sys.stdout, index=False)
 
 
 
